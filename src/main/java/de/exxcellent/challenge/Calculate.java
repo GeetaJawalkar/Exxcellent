0cec45f3426f5843fb0d2661ae0e360e953ebd3e
Geeta <geetu.mj@gmail.com>
2019-03-03 18:04:18 +0100

    calculate difference implementation

--- src/main/java/de/exxcellent/challenge/Calculate.java
+++ src/main/java/de/exxcellent/challenge/Calculate.java
@@ -1,6 +1,16 @@
 package de.exxcellent.challenge;
 
 import java.util.Arrays;
+import java.util.List;
+
+class Result{
+	public final Integer leastDiff;
+	public final String tag;
+	public Result(Integer diff, String name) {
+		this.leastDiff = diff;
+		this.tag = name;
+	}
+}
 
 public class Calculate {
 
@@ -9,9 +19,37 @@ public class Calculate {
 		int par1 = Arrays.asList(firstLine).indexOf(interested_params[0] );
 		int par2 = Arrays.asList(firstLine).indexOf(interested_params[1] );
 		int par3 = Arrays.asList(firstLine).indexOf(interested_params[2] );
-		System.out.println(par1 + " " +  par2 + " " + par3);
-		
+
 		int[] result = {par1, par2, par3};
 		return result;
+	}
+	
+	
+	Result calculateDiff(List<String[]> dataList, int[] indexes) {
+		
+		Integer diff, minDiff = 0;
+		String minDiffVar=null;
+
+		int key0 = indexes[0];
+		int key1 = indexes[1];
+		int key2 = indexes[2];
+		
+		String[] dataString = dataList.get(key0);
+
+		diff = Math.abs( Integer.valueOf(dataString[key1]) - Integer.valueOf(dataString[key2]) );
+		minDiff = diff;
+		
+		for (int i = 1; i < dataList.size(); i++) {
+			
+		    dataString = dataList.get(i);
+		    diff = Math.abs( Integer.valueOf(dataString[key1]) - Integer.valueOf(dataString[key2]) );
+		    
+		    if(diff<minDiff) {		    	
+		    	minDiff = diff; 
+		    	minDiffVar = dataString[0];	    	
+		    } 	   
+		}
+
+		return new Result(minDiff, minDiffVar);
 	}
 }
--- src/test/java/de/exxcellent/challenge/CalculateTest.java
+++ src/test/java/de/exxcellent/challenge/CalculateTest.java
@@ -2,6 +2,9 @@ package de.exxcellent.challenge;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class CalculateTest {
@@ -18,5 +21,22 @@ class CalculateTest {
 		assertEquals(1, result[1]);
 		assertEquals(2, result[2]);
 	}
-
+	
+	@Test
+	public void test_calculateDiff() {
+		Calculate cal = new Calculate();
+		
+		String[] data0 = {"Day", "MxT", "MnT"};
+		String[] data1 = {"Day1", "10", "20"};
+		String[] data2 = {"Day2", "-2", "20"};
+		
+		List<String[]> dataList = new ArrayList<String[]>();
+		dataList.add(data2);
+		dataList.add(data1);
+		
+		int[] indexes = {0, 1, 2};
+		Result result = cal.calculateDiff(dataList, indexes);
+		assertEquals(10, (int)result.leastDiff);
+		assertEquals("brand1", result.tag);
+	}
 }
